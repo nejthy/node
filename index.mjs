cc4657d9be96a4b3cfbadc169508b8d71169afbe
kuza <kuzn03@vse.cz>
2025-03-11 18:48:03 +0100

    ukol 2

--- index.mjs
+++ index.mjs
@@ -1,36 +1,34 @@
-import { rejects } from 'assert';
-import { error } from 'console';
-import fs from 'fs';
-import { resolve } from 'path';
+import { readFile, writeFile } from 'fs/promises';
+
+const writeAndReadFile = async () => {
+
+    try {
+        const instrukce = await readFile('instrukce.txt', 'utf-8')
+        const n = parseInt(instrukce.trim())
+
+        if (isNaN(n)) {
+            throw new error('Instrukce neobsahují číslo')
+        }
+
+        const promises = [];
+        for (let i=0; i <= n; i++) {
+            const fileName = `${i}.txt`
+            const content = `Soubor ${i}`
+            promises.push(writeFile(fileName, content));
+        }
+
+        await Promise.all(promises);
+
+        if (n <= 4 ) {
+            console.log(`Úspěšně vytvořeny ${n + 1} soubory`) 
+        }else {
+            console.log(`Úspěšně vytvořeno ${n + 1} souborů`) 
+        }
+
+    } catch(error) {
+        console.error('Chyba:', error);
+    }
+};
 
 
-const readFile = (name) => {
-    return new Promise((resolve, reject) =>{
-        fs.readFile(name, (error, data) => {
-            if (error) {
-                reject(error)
-            } else {
-                resolve(data.toString())
-            }
-        })
-    }) 
-}
-const writeFile = (name, data) => (name, data) => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(name, data, (error) => {
-           if (error) {
-            reject(error)
-           } else {
-            resolve()
-           }
-        })
-    })
-}
-readFile('instrukce.txt').then((instrukce) => {
-    const [vstup, vystup] = instrukce.split(' ')
-
-    return readFile(vstup).then((obsahVstupu) => {
-        return writeFile(vystup, obsahVstupu)
-    })
-})
-
+writeAndReadFile();
